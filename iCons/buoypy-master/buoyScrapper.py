7f17e2cf07474047219669b61b5f7c472b54f0c3
AntonioEscallon <66785155+AntonioEscallon@users.noreply.github.com>
2022-03-02 09:12:28 -0500

    Trying to fix the gitignore

--- iCons/buoypy-master/buoyScrapper.py
+++ iCons/buoypy-master/buoyScrapper.py
@@ -5,11 +5,12 @@ buoy = 41108 #wilmington harbor
 B = bp.realtime(buoy) #wilmington harbor
 
 df = B.txt()
+print(df)
 
 # plotting
 fig,ax = plt.subplots(2,sharex=True)
 df.WVHT.plot(ax=ax[0])
-ax[0].set_ylabel('Wave Height (m)',fontsize=14)
+ax[0].set_ylabel('Wind Speed (m/s)',fontsize=14)
 
 df.DPD.plot(ax=ax[1])
 ax[1].set_ylabel('Dominant Period (sec)',fontsize=14)
